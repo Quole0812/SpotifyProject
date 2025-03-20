d7ecd6134948dfd5c45137a70ee1fd2bef7ea0c4
Jackson Le <wnj9tx@virgnia.edu>
2025-03-19 20:46:08 -0400

    base convert function, will still have to doublecheck format

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-# getting data
-
-
-
-
-#setting up api call, need access token first
+# file converter, could fix if result is not desired later https://stackoverflow.com/questions/1871524/how-can-i-convert-json-to-csv
+import panda as pd
 import requests
 import base64
 
-#these are normally valid for 1 hr so u guys can make ur own app and just paste these info here to generate more token
-client_id = 'eae2627823e2419890fa69ae50deec74' #this is jackson's info plz dont do bad thing
+
+def jsonToCSV(json_file_name, output_csv="csvfile.csv"):
+    with open(json_file_name, encoding='utf-8') as inputfile:
+        df = pd.read_json(inputfile)
+
+    df.to_csv(output_csv, encoding='utf-8', index=False)
+
+
+# https://stackoverflow.com/questions/19697846/how-to-convert-csv-file-to-multiline-json
+def csvToJson(csv_file_name, output_csv="jsonfile.json"):
+    csv_file = pd.DataFrame(pd.read_csv(csv_file_name, sep=",", header=0, index_col=False))
+    csv_file.to_json(output_csv)
+
+
+# setting up api call, need access token first
+# these are normally valid for 1 hr so u guys can make ur own app and just paste these info here to generate more token
+client_id = 'eae2627823e2419890fa69ae50deec74'  # this is jackson's info plz dont do bad thing
 client_secret = '8f884de0f9d04452a3116fcdbceb8074'
 
 url = "https://api.spotify.com/v1/"
+
 
 # kinda mimiced this
 # https://developer.spotify.com/documentation/web-api/tutorials/client-credentials-flow
@@ -43,5 +55,3 @@ def get_api_token():
 # test_token = get_api_token() #run this every 1 hr to get ur token lol
 
 token = "BQBJ4ejUWPTP4iJDpbALxoiivXil99DVmSUWdwmM9MeJbHN91G4FPfcjT6kK67Fe3nS_t7RFn9gaUnbYYu87USG05VGF802loSJgvXKBfv787Q5dS03j6ybvB5-_dV1S1FuMJEemyxs"
-
-
